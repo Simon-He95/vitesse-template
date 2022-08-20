0c4922311f48a68bc025ad51f84b2ee0a696bc10
Simon-He95 <674949287@qq.com>
2022-08-20 10:57:48 +0800

    chore: update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,6 +1,11 @@
+<script setup>
+import { Hello } from '../../dist/index'
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <router-view />
+    <!-- <router-view /> -->
+    <Hello />
     <Footer />
   </main>
 </template>
